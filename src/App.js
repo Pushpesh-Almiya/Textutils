2e0da6a613e3b46b81b5e327952836ced7db449a
Pushpesh Almiya <almiyapushpesh55@gmail.com>
2023-06-28 09:57:21 +0530

    Dark mode function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Main from './components/Main';
+import Navbar from './components/Navbar';
 function App() {
+  const [mode , setMode]= useState("light")
+  const toggleMode =()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor="#021f3b"
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor="white"
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar title ="Textutils" mode={mode} toggleMode={toggleMode}/>
+    <Main heading ="Textutils-Play with your text" mode={mode}/>
+    </>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,65 @@
+import { React, useState } from "react";
+
+export default function Main(props) {
+  const [text, setText] = useState("Enter your text");
+  const enterText = (e) => {
+    setText(e.target.value);
+  };
+  const uppercase = () => {
+    setText(text.toUpperCase());
+  };
+  const lowercase = () => {
+    setText(text.toLowerCase());
+  };
+  const clearText = () => {
+    setText("");
+  };
+  const copyText = () => {
+    let enterText = document.getElementById('textarea')
+    enterText.select();
+    navigator.clipboard.writeText(text);
+  };
+  const removeExtraSpaces =()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(' '))
+  }
+  return (
+    <>
+    <div   className="mainBody"style={{color:props.mode==="dark"?"white":"black"}} >
+      <div  className="container">
+        <h1 className="my-3">{props.heading}</h1>
+        <textarea  id="textarea" style={{backgroundColor:props.mode==='dark'?"#021f3b":"white", color:props.mode==="dark"?"white":"black"}}
+          className="p-3"
+          name="box"
+          value={text}
+          onChange={enterText}
+          cols="120"
+          rows="8"
+        ></textarea>
+        <button className="btn btn-primary m-2" onClick={uppercase}>
+          Convert to UPPERCASE
+        </button>
+        <button className="btn btn-success m-2" onClick={lowercase}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-danger m-2" onClick={clearText}>
+          Clear Text
+        </button>
+        <button className="btn btn-warning m-2" onClick={copyText}>
+          Copy Text
+        </button>
+        <button className="btn btn-info m-2" onClick={removeExtraSpaces}>
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div className="container">
+      <h3>Your Text Summary</h3>
+      <p>{text.split("").length} words and {text.length} characters</p>
+      <h3>Preview</h3>
+      <p>{text.length>0 ?text:"Please enter text to preview"}</p>
+      </div>
+      </div>
+
+    </>
+  );
+}
